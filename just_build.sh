a22c91375ae4c71ac560d8873b935605c9a6a415
Luca Weiss <luca@z3ntu.xyz>
2018-05-28 11:50:14 +0200

    Update kernel build script

--- just_build.sh
+++ just_build.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-cd kernel
-export PATH=../../arm-linux-androideabi-4.6/bin:$PATH
+
+set -e
+
+cd alps-fairphone-gpl/kernel
+#export PATH=~/android/prebuilt/arm-linux-androideabi-4.6/bin:$PATH
+#export PATH=~/android/prebuilt/arm-linux-androideabi-4.7/bin:$PATH
+#export PATH=~/android/prebuilt/arm-linux-androideabi-4.8/bin:$PATH
+export PATH=~/android/prebuilt/arm-linux-androideabi-4.9/bin:$PATH
+export PATH=~/android/prebuilt/compat:$PATH
 export CROSS_COMPILE=arm-linux-androideabi-
 echo "Compiling"
 TARGET_BUILD_VARIANT=user \
-    TARGET_PRODUCT=ahong89_wet_jb2 \
-    MTK_ROOT_CUSTOM=../mediatek/custom/ \
-    make V=1
+TARGET_PRODUCT=ahong89_wet_jb2 \
+MTK_ROOT_CUSTOM=../mediatek/custom/ \
+make fp1_defconfig # only for Dan's tree
+make -j8 #V=1 #CONFIG_DEBUG_SECTION_MISMATCH=y
 echo "Hopefully the compilation worked without problems!"
 echo "Made by z3ntu"
